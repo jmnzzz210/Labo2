324490210b736941e887d8a3ca6fb417aa0bb33e
Brandon <brandon.jimenezcampos@ucr.ac.cr>
2025-03-29 15:08:55 -0600

    agrego de comentarios ejercicio3.sh

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-directorio="/home/brandon/Downloads"
-monitor="/home/brandon/Documents/Laboratorios/Laboratorio2/acciones.log"
+directorio="/home/brandon/Downloads" #se selecciona el directorio
+monitor="/home/brandon/Documents/Laboratorios/Laboratorio2/acciones.log" #se asigna la ruta absoluta del archivo .log
 
-if [ -d $directorio ]; then
-        inotifywait -m -e create,modify,delete,move "$directorio" |
+if [ -d $directorio ]; then 
+        inotifywait -m -e create,modify,delete,move "$directorio" | #se coloca que este monitoreando constantemente creaciones, modificaciones, eliminaciones y cambios
                 while read -r dir accion archivo; do
                 echo "[$(date '+%d/%m/%y %H:%M:%S')] Accion ocurrida: $accion en el archivo $archivo">>"$monitor"
 done
